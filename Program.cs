aea4ec056c3df3fb10666ee53311a226d97a99fc
Olexandr Pascal <futupas@gmail.com>
2023-02-12 13:01:42 +0000

    add: first commit (project setup)

---
+++ Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.WebHost.ConfigureKestrel((context, serverOptions) =>
+{
+    var kestrelSection = context.Configuration.GetSection("Kestrel");
+    serverOptions.Configure(kestrelSection);
+    //serverOptions.Configure(kestrelSection)
+    //    .Endpoint("HTTPS", listenOptions =>
+    //    {
+    //        // ...
+    //    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (true || app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
